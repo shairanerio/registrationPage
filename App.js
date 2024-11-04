c39c193f749770c97bcfeecc1f7758a3e7d764d0
unknown <Shaira Nerio>
2024-11-04 18:40:32 +0800

    Initial commit

--- App.js
+++ App.js
@@ -1,72 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Provider as PaperProvider, TextInput, Button, Divider } from 'react-native-paper';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {useState} from 'react';
 
 export default function App() {
-    const [isChecked, setIsChecked] = useState(false);
-  
-  const toggleCheck = () => {
-    setIsChecked(!isChecked);
-  }
-  
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={styles.container}>
+    <PaperProvider>
+      <View style={styles.container}>
         <StatusBar style="auto" />
 
-      <View style={styles.regSection}>
-        <Text style={[styles.regTitle]}>Registration Form</Text>
-      </View>
-
-      <View style={styles.nameSection}>
-        <View style={styles.nameText}>
-          <Text>Full Name:</Text>
-          <TextInput style={styles.input} placeholder="Full Name" keyboardType="default" autoCapitalize="none"></TextInput>
+        <View style={styles.regSection}>
+          <Text style={styles.regTitle}>Registration Form</Text>
         </View>
-      </View>
 
-      <View style={styles.emAddSection}>
-        <View style={styles.emAddText}>
-          <Text>Email Address:</Text>
-          <TextInput style={styles.input} placeholder='Enter email' keyboardType='email-address' autoCapitalize='none'></TextInput>
+        <TextInput label="Full Name" value={fullName} onChangeText={text => setFullName(text)} style={styles.input}/>
+
+        <TextInput label="Email Address" value={email} onChangeText={text => setEmail(text)} style={styles.input} keyboardType="email-address"/>
+
+        <TextInput label="Password" value={password} onChangeText={text => setPassword(text)} style={styles.input} secureTextEntry/>
+
+        <Button mode="contained" style={styles.signUpButton} onPress={() => console.log('Sign Up pressed')} labelStyle={styles.signUpButtonText}>Sign Up</Button>
+
+        <View style={styles.haveAccSection}>
+          <Text style={styles.haveAccText}>Already have an account?</Text>
+          <Button onPress={() => console.log('Login')} labelStyle={styles.logInButtonText}>LOGIN</Button>
         </View>
-      </View>
 
-      <View style={styles.passSection}>
-        <View style={styles.passText}>
-          <Text>Password:</Text>
-          <TextInput style={styles.input} placeholder='Enter password' secureTextEntry={true} autoCapitalize='none'></TextInput>
+        <View style={styles.orSection}>
+          <Divider style={styles.line} />
+          <Text style={styles.orText}>OR</Text>
+          <Divider style={styles.line} />
         </View>
+
+        <Button icon={() => <FontAwesome name="google" size={24} style={styles.icon} />} mode="outlined" style={styles.socialButton} labelStyle={styles.socialButtonText} onPress={() => console.log('Sign Up with Google pressed')}>Sign Up with Google</Button>
+
+        <Button icon={() => <FontAwesome name="facebook" size={24} style={styles.icon} />} mode="outlined" style={styles.socialButton} labelStyle={styles.socialButtonText} onPress={() => console.log('Sign Up with Facebook pressed')}>Sign Up with Facebook</Button>
       </View>
-
-      <TouchableOpacity style={styles.signUpButton} onPress={() => console.log('Sign Up pressed')}>
-        <Text style={styles.signUpButtonText}>Sign Up</Text>
-      </TouchableOpacity>
-
-      <View style={styles.haveAccSection}>
-        <Text style={styles.haveAccText}>Already have an account?</Text>
-          <TouchableOpacity onPress={() => console.log('Already Have an Account pressed')}>
-            <Text style={styles.LogInButtonText}>LOGIN</Text>
-          </TouchableOpacity>
-      </View>
-
-      <View style={styles.orSection}>
-        <View style={styles.line}></View>
-        <Text style={[styles.orText]}>OR</Text>
-        <View style={styles.line}></View>
-      </View>
-
-      <TouchableOpacity style={styles.googleButton} onPress={() => console.log('Login with Google pressed')}>
-        <FontAwesome name="google" size={24} color="#AB644B" />
-        <Text style={styles.googleButtonText}>Sign Up with Google</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.facebookButton} onPress={() => console.log('Login with Facebook pressed')}>
-        <FontAwesome name="facebook" size={24} color="#AB644B" />
-        <Text style={styles.googleButtonText}>Sign Up with Facebook</Text>
-      </TouchableOpacity>
-    </View>
+    </PaperProvider>
   );
 }
 
@@ -74,67 +49,50 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#8ecae6',
+    paddingHorizontal: 20,
+    justifyContent: 'center',
   },
   regSection: {
     alignItems: 'center',
-    marginTop: 75,
+    marginBottom: 20,
   },
   regTitle: {
     fontWeight: 'bold',
     fontSize: 25,
   },
-  nameText: {
-    marginLeft: 25,
-    marginTop: 40,
-  },
   input: {
-    borderWidth: 1,
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingLeft: 5,
-    paddingRight: 5,
-    width: 307,
-    marginTop: 10,
-    borderRadius: 10,
-  },
-  emAddText: {
-    marginLeft: 25,
-    marginTop: 10,
-  },
-  passText: {
-    marginLeft: 25,
-    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: 'transparent',
   },
   signUpButton: {
-    padding: 15,
-    marginTop: 35,
+    marginVertical: 20,
     backgroundColor: '#AB644B',
     borderRadius: 10,
-    width: 250,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: 53,
   },
   signUpButtonText: {
     color: '#fff',
   },
-  haveAccButton: {
-    marginLeft: 100,
-    marginTop: 8,
+  haveAccSection: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
   },
-  haveAccButtonText: {
+  haveAccText: {
+    fontSize: 16,
     color: '#696969',
+    marginRight: -8,
   },
-  LogInButtonText: {
+  logInButtonText: {
     fontSize: 16,
     color: '#AB644B',
     fontWeight: 'bold',
+    marginTop: 12,
   },
   orSection: {
     flexDirection: 'row',
     alignItems: 'center',
     marginVertical: 20,
-    marginTop: 20,
   },
   orText: {
     marginHorizontal: 10,
@@ -146,49 +104,15 @@ const styles = StyleSheet.create({
     height: 1,
     backgroundColor: 'black',
   },
-  googleButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 5,
-    paddingVertical: 10,
-    borderWidth: 1,
+  socialButton: {
+    marginVertical: 5,
     borderColor: '#AB644B',
-    borderRadius: 5,
-    width: 250,
-    alignSelf: 'center',
+    borderRadius: 10,
   },
-  googleButtonText: {
-    marginLeft: 10,
+  socialButtonText: {
     color: '#AB644B',
-    fontSize: 16,
   },
-  facebookButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 15,
-    paddingVertical: 10,
-    borderWidth: 1,
-    borderColor: '#AB644B',
-    borderRadius: 5,
-    width: 250,
-    alignSelf: 'center',
-  },
-  facebookButtonText: {
-    marginLeft: 10,
+  icon: {
     color: '#AB644B',
-    fontSize: 16,
-  },
-  haveAccSection: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  haveAccText: {
-    fontSize: 16,
-    color: '#696969',
-    marginRight: 2,
   },
 });
